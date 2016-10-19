a557f5b75872a0d674753413eed2cd98bb6cbe3d
John Hinton <jh011889@gmail.com>
2016-10-19 06:59:05 -0400

    remove unused imports

--- app/src/test/java/com/sample/johns/weatherapp/PresenterTest.java
+++ app/src/test/java/com/sample/johns/weatherapp/PresenterTest.java
@@ -15,10 +15,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.io.IOException;
-
-import okhttp3.Protocol;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Call;
 import retrofit2.http.Query;
 import retrofit2.mock.Calls;
